af1dcacfcfc602e67de62df3c7fd3924bf9137be
zhuge06666 <897126541@qq.com>
2018-05-20 11:44:44 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/gdmec/android/viewpagerdemo/MainActivity.java
@@ -0,0 +1,121 @@
+package cn.edu.gdmec.android.viewpagerdemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+private ViewPager vp;
+private View view1,view2,view3;
+private MyAdapter adapter;
+TextView tv1,tv2,tv3;
+private List<View> list;
+private Handler handler;
+private Thread thread;
+private boolean flag=true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vp=findViewById(R.id.vp);
+        tv1=findViewById(R.id.tv1);
+        tv2=findViewById(R.id.tv2);
+        tv3=findViewById(R.id.tv3);
+        tv1.setOnClickListener(this);
+        tv2.setOnClickListener(this);
+        tv3.setOnClickListener(this);
+        LayoutInflater layoutInflater=LayoutInflater.from(this);
+        view1=layoutInflater.inflate(R.layout.page1,null);
+        view2=layoutInflater.inflate(R.layout.page2,null);
+        view3=layoutInflater.inflate(R.layout.page3,null);
+        list=new ArrayList<View>();
+        list.add(view1);
+        list.add(view2);
+        list.add(view3);
+        adapter=new MyAdapter(list);
+        vp.setAdapter(adapter);
+        tv1.setTextColor(Color.parseColor("#FF6600"));
+        tv2.setTextColor(Color.parseColor("#050505"));
+        tv3.setTextColor(Color.parseColor("#050505"));
+        handler=new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what==1){
+                    if (adapter.getCount()>0){
+                        if (vp.getCurrentItem()==list.size()-1){
+                            tv1.setTextColor(Color.parseColor("#FF6600"));
+                            tv2.setTextColor(Color.parseColor("#050505"));
+                            tv3.setTextColor(Color.parseColor("#050505"));
+                            vp.setCurrentItem(0);
+                        }else if (vp.getCurrentItem()==0){
+                                tv2.setTextColor(Color.parseColor("#FF6600"));
+                                tv1.setTextColor(Color.parseColor("#050505"));
+                                tv3.setTextColor(Color.parseColor("#050505"));
+                            vp.setCurrentItem(vp.getCurrentItem()+1);
+                            }else if(vp.getCurrentItem()==1){
+                                tv3.setTextColor(Color.parseColor("#FF6600"));
+                                tv1.setTextColor(Color.parseColor("#050505"));
+                                tv2.setTextColor(Color.parseColor("#050505"));
+                            vp.setCurrentItem(vp.getCurrentItem()+1);
+                            }
+
+                        }
+                    }
+                }
+
+        };
+        setViewPager();
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv1:
+
+                vp.setCurrentItem(0);
+                tv1.setTextColor(Color.parseColor("#FF6600"));
+                tv2.setTextColor(Color.parseColor("#050505"));
+                tv3.setTextColor(Color.parseColor("#050505"));
+                break;
+            case R.id.tv2:
+                vp.setCurrentItem(1);
+                tv2.setTextColor(Color.parseColor("#FF6600"));
+                tv1.setTextColor(Color.parseColor("#050505"));
+                tv3.setTextColor(Color.parseColor("#050505"));
+                break;
+            case R.id.tv3:
+                vp.setCurrentItem(2);
+                tv3.setTextColor(Color.parseColor("#FF6600"));
+                tv1.setTextColor(Color.parseColor("#050505"));
+                tv2.setTextColor(Color.parseColor("#050505"));
+                break;
+        }
+    }
+    private void setViewPager(){
+        thread=new Thread(){
+            @Override
+            public void run() {
+                while (flag){
+                    try {
+                        sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Message msg=new Message();
+                    msg.what=1;
+                    handler.sendMessage(msg);
+                }
+            }
+        };
+        thread.start();
+    }
+
+}
---
+++ app/src/main/java/cn/edu/gdmec/android/viewpagerdemo/MyAdapter.java
@@ -0,0 +1,39 @@
+package cn.edu.gdmec.android.viewpagerdemo;
+
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/5/20.
+ */
+
+public class MyAdapter extends PagerAdapter {
+     private List<View> list;
+    public MyAdapter(List<View> list) {
+           this.list=list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(list.get(position));
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view==object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        container.addView(list.get(position));
+        return list.get(position);
+    }
+}
